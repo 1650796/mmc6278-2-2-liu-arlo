758e6eccc1fa6eceddf270ce0c99636e19d7e470
catsociety <aliu6876@gmail.com>
2023-05-28 02:30:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -12,15 +12,41 @@ program
   .command("getQuote")
   .description("Retrieves a random quote")
   .action(async () => {
+    try {
+      const quotes = await fs.readFile(QUOTE_FILE, 'utf-8')
+      const splitQuotes = quotes.split('\n')
+      const randomLine = splitQuotes[Math.floor(Math.random()*splitQuotes.length)]
+      const splitAuthor = randomLine.split ('|')
+      
+      console.log(splitAuthor)
+
+    } catch (err) {
+      console.log(err)
+    }
+    });
+  
     // TODO: Pull a random quote from the quotes.txt file
     // console log the quote and author
     // You may style the text with chalk as you wish
-  });
+  
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
+
+    try {
+      const addQuote = `${quote}`
+      const addAuthor = `${author || "Anonymous"}`
+      const addedLine = `\n + ${quote} | ${author}`
+      await fs.appendFile(QUOTE_FILE, addedLine, 'utf-8')
+
+      console.log('Your quote has been added to the file.')
+
+    } catch (err) {
+      console.log(err)
+    }
+  });
     // TODO: Add the quote and author to the quotes.txt file
     // If no author is provided,
     // save the author as "Anonymous".
@@ -29,6 +55,6 @@ program
     // You may style the text with chalk as you wish
     // HINT: You can store both author and quote on the same line using
     // a separator like pipe | and then using .split() when retrieving
-  });
+  
 
 program.parse();
